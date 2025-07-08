0880baa01872d9f9ad4235b996c47df843ba43ca
Robert Gleim <rgleim@outlook.com>
2025-07-08 13:10:51 -0500

    added a error catch

--- personalbudget.py
+++ personalbudget.py
@@ -1,8 +1,14 @@
+def get_float_input(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Invalid input! Please enter a number.")
 
-monthly_income = float(input("Enter your monthly budget: "))
-rent = float(input("Enter your monthly rent: "))
-food = float(input("Enter your monthly food expenses: "))
-entertainment = float(input("Enter your monthly entertainment expenses: "))
+monthly_income = get_float_input("Enter your monthly income: ")
+rent =  get_float_input("Enter your monthly rent: ")
+food = get_float_input("Enter your monthly food expenses: ")
+entertainment = get_float_input("Enter your monthly entertainment expenses: ")
 total_expenses = rent + food + entertainment
 remaining_budget = monthly_income - total_expenses
 
